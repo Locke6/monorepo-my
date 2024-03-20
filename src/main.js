36f402dce2451ff2277509d52e386890a79d7e98
unknown <hezhenkang@zhixueyun.com>
2024-03-20 23:46:38 +0800

    new update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 
 // 创建相机
 const camera = new THREE.PerspectiveCamera(
-    45, // 视角
+    44, // 视角
     window.innerWidth / window.innerHeight, // 宽高比
     0.1, // 近平面
     1000 // 远平面内
